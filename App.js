f4efe01a8b71b89629679c08c16f962ec5f22b52
HerbertonMiguel-Dev <herbertonmiguel.dev@gmail.com>
2024-07-05 11:41:46 -0300

    foi adicionado o uso do ActivityIndicator

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import { View, Text, StyleSheet, FlatList } from "react-native";
+import { View, Text, StyleSheet, FlatList, ActivityIndicator } from "react-native";
 
 import api from './src/services/api';
 import Filmes from './src/Filmes';
@@ -7,6 +7,8 @@ import Filmes from './src/Filmes';
 export default function App() {
   // Estado para armazenar a lista de filmes
   const [filmes, setFilmes] = useState([]);
+  // Estado para gerenciar o carregamento
+  const [loading, setLoading] = useState(true);
 
   // useEffect para carregar os filmes ao montar o componente
   useEffect(() => {
@@ -17,22 +19,35 @@ export default function App() {
 
       // Atualiza o estado com os dados da resposta
       setFilmes(response.data);
+      // Define o estado de carregamento como false após carregar os dados
+      setLoading(false);
     }
 
     // Chama a função para carregar os filmes
     loadFilmes();
   }, []); // Array de dependências vazio para executar apenas uma vez
 
-  return (
-    <View style={styles.container}>
-      {/* FlatList para renderizar a lista de filmes */}
-      <FlatList 
-        data={filmes} // Fonte de dados da lista
-        keyExtractor={item => String(item.id)} // Função para extrair a chave de cada item
-        renderItem={({ item }) => <Filmes data={item}/>} // Função para renderizar cada item da lista
-      />
-    </View>
-  );
+  // Verifica se ainda está carregando
+  if (loading) {
+    // Retorna um indicador de atividade enquanto os dados estão sendo carregados
+    return (
+      <View style={{ alignItems: 'center', justifyContent: 'center', flex: 1 }}>
+        <ActivityIndicator color="#121212" size={45} />
+      </View>
+    );
+  } else {
+    // Retorna a lista de filmes após os dados serem carregados
+    return (
+      <View style={styles.container}>
+        {/* FlatList para renderizar a lista de filmes */}
+        <FlatList 
+          data={filmes} // Fonte de dados da lista
+          keyExtractor={item => String(item.id)} // Função para extrair a chave de cada item
+          renderItem={({ item }) => <Filmes data={item} />} // Função para renderizar cada item da lista
+        />
+      </View>
+    );
+  }
 }
 
 // Estilos para os componentes
